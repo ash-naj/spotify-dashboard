36a7ae899ce9f7553d1f3a6023d41e5e4e61477e
ash-naj <109481137+ash-naj@users.noreply.github.com>
2026-04-08 19:41:24 +0200

    Remove ASCII creator from GitHub and Docker

--- ASCII creator.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('CSV exports/ASCII.csv')
-
-df['track'] = df['track'].str.slice(0, 15) + '...'
-mini_df = df[['track', 'artist', 'hours_listened']]
-
-print(mini_df.to_markdown(index=False))
